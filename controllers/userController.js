04a057e71a3131a43a33ef273c6e85aeaff7801e
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-08-30 10:03:59 +0530

    added

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,7 +47,7 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
   res
     .status(201)
     .cookie("token", "", {
-      httpOnly: true,
+      httpsOnly: true,
       expires: new Date(Date.now()),
     })
     .json({
